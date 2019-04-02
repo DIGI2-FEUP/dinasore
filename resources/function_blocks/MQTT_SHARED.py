9486a2809addfbe72c4b3d50152b57c9fd16b1d0
eliseu <eliseu@fe.up.pt>
2019-04-02 19:11:25 +0100

    mqtt shared client

--- resources/function_blocks/MQTT_SHARED.py
+++ resources/function_blocks/MQTT_SHARED.py
@@ -14,7 +14,7 @@ class SharedResources:
     def connect(self, host, port):
 
         def on_message(client, userdata, message):
-            print('new message: {0}\ntopic: {1}'.format(message.payload, message.topic))
+            # print('new message: {0}\ntopic: {1}'.format(message.payload, message.topic))
             self.topic = message.topic
             self.message_payload = message.payload
             self.new_message.set()
@@ -30,11 +30,11 @@ class MQTT_SHARED:
     def schedule(self, event_name, event_value, topic, host, port):
         if event_name == 'INIT':
             self.resources.connect(host, port)
-            return [event_value, None, None, 0.0, 0.0]
+            return [event_value, None, None, None, 0.0, 0.0]
 
         elif event_name == 'SUBS':
             self.resources.client.subscribe(topic)
-            return [None, event_value, None, 0.0, 0.0]
+            return [None, event_value, None, None, 0.0, 0.0]
 
         elif event_name == 'READ':
             self.resources.new_message.wait()
@@ -45,20 +45,20 @@ class MQTT_SHARED:
                 self.resources.new_message.clear()
                 humidity = payload_json['S1TH']['Humidity']
                 temperature = payload_json['S1TH']['Temperature']
-                return [None, None, event_value, humidity, temperature]
+                return [None, None, event_value, None, humidity, temperature]
             else:
                 self.schedule('READ', event_value, topic, host, port)
 
         elif event_name == 'STOP':
             self.resources.client.disconnect()
-            return [None, None, None, 0.0, 0.0]
+            return [None, None, None, event_value, 0.0, 0.0]
 
 
-c = MQTT_SHARED()
-c.schedule('INIT', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
-c.schedule('SUBS', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
-v = c.schedule('READ', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
-print(v)
-v = c.schedule('READ', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
-print(v)
+# c = MQTT_SHARED()
+# c.schedule('INIT', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
+# c.schedule('SUBS', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
+# v = c.schedule('READ', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
+# print(v)
+# v = c.schedule('READ', 1, 'Fucoli/ISQsensor1/sensor', 'localhost', 1883)
+# print(v)
 # {"Time":"2019-04-01T17:46:26", "S1TH":{"Temperature":20.7, "Humidity":53.8},"TempUnit":"C"}
