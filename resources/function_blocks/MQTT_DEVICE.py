4e4665673fdc6e159ef00d83de9898dc449dc2bb
eliseu <eliseu@fe.up.pt>
2019-07-10 18:43:29 +0100

    mqtt corrections

--- resources/function_blocks/MQTT_DEVICE.py
+++ resources/function_blocks/MQTT_DEVICE.py
@@ -1,5 +1,4 @@
 from threading import Event
-from threading import Lock
 from paho.mqtt.client import Client
 import json
 
@@ -11,15 +10,12 @@ class SharedResources:
         self.topic = None
         self.message_payload = None
         self.new_message = Event()
-        self.stop = Event()
-        self.read_message = Lock()
 
     def connect(self, host, port):
         self.client = Client()
 
         def on_message(client, userdata, message):
             # print('new message: {0}\ntopic: {1}'.format(message.payload, message.topic))
-            self.read_message.acquire()
             self.topic = message.topic
             self.message_payload = message.payload
             self.new_message.set()
@@ -34,6 +30,7 @@ class MQTT_DEVICE:
 
     def __init__(self):
         self.resources.new_message.clear()
+        self.stop = Event()
 
     def schedule(self, event_name, event_value, topic, host, port):
         if event_name == 'INIT':
@@ -43,7 +40,7 @@ class MQTT_DEVICE:
 
         elif event_name == 'READ':
             # wait for new messages
-            while not self.resources.stop.is_set():
+            while not self.stop.is_set():
                 self.resources.new_message.wait()
 
                 # checks if is the right topic
@@ -54,21 +51,18 @@ class MQTT_DEVICE:
                     value = payload_json['temperature_value']
                     # otherwise clears the new message event
                     self.resources.new_message.clear()
-                    self.resources.read_message.release()
                     return [None, event_value, None, value]
 
                 # otherwise clears the new message event
                 # self.resources.new_message.clear()
 
         elif event_name == 'STOP':
-            self.resources.stop.set()
-            self.resources.read_message.release()
+            self.stop.set()
             self.resources.new_message.clear()
             self.resources.client.disconnect()
             return [None, None, event_value, 0.0]
 
     def __del__(self):
-        self.resources.stop.set()
-        self.resources.read_message.release()
+        self.stop.set()
         self.resources.new_message.clear()
         self.resources.client.disconnect()
